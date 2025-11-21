908e58424c9538d8c923b7e1aed6ba950b399dc8
roll-raihan <raihanahmednasim@gmail.com>
2025-11-21 08:28:44 +0600

    modified banner for smaller device

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -10,12 +10,12 @@ import { FaArrowCircleRight } from 'react-icons/fa';
 const Banner = () => {
     return (
         <Carousel
-        autoPlay={true}
-        infiniteLoop={true}
+        // autoPlay={true}
+        // infiniteLoop={true}
         >
             <div className='relative'>
                 <img src={bannerImg1} />
-                <div className='flex justify-center items-center gap-5 absolute bottom-8 lg:bottom-20 left-8 lg:left-20'>
+                <div className='flex justify-center items-center gap-5 absolute bottom-0 left-8 md:bottom-8 lg:bottom-20 lg:left-20'>
                     <button className='rounded-2xl bg-primary p-2 flex justify-center items-center gap-2 font-bold'>Track Your Parcel <FaArrowCircleRight /></button>
                     <button className='btn font-bold'>Be A Rider</button>
                 </div>
