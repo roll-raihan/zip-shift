faa5b43225a9d1df7955361a21a5fffe6a4873c1
roll-raihan <raihanahmednasim@gmail.com>
2025-11-29 12:28:31 +0600

    added cost functionality for parcel weight

--- src/pages/SendParcel/SendParcel.jsx
+++ src/pages/SendParcel/SendParcel.jsx
@@ -22,6 +22,28 @@ const SendParcel = () => {
 
     const handleSendParcel = (data) => {
         console.log(data);
+
+        const isDocument = data.parcelType === 'document';
+        const isSameDistrict = data.senderDistrict === data.receiverDistrict;
+        const parcelWeight = parseFloat(data.parcelWeight);
+        // console.log(isSameDistrict);
+
+        let cost = 0;
+        if (isDocument) {
+            cost = isSameDistrict ? 60 : 80;
+        }
+        else {
+            if (parcelWeight <= 3) {
+                cost = isSameDistrict ? 110 : 150;
+            }
+            else {
+                const minCharge = isSameDistrict ? 110 : 150;
+                const extraWeight = parcelWeight - 3;
+                const extraCharge = isSameDistrict ? extraWeight * 40 : extraWeight * 40 + 40;
+                cost = minCharge + extraCharge;
+            }
+        }
+        console.log('cost',cost);
     }
 
     return (
