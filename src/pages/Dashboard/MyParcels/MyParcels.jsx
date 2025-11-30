554893f3df85b405ebd96442db0b420a74de30c1
roll-raihan <raihanahmednasim@gmail.com>
2025-11-30 19:07:19 +0600

    added axios delete option in client

--- src/pages/Dashboard/MyParcels/MyParcels.jsx
+++ src/pages/Dashboard/MyParcels/MyParcels.jsx
@@ -2,19 +2,56 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import UseAuth from '../../../hooks/UseAuth';
 import UseAxiosSecure from '../../../hooks/UseAxiosSecure';
+import { FaMagnifyingGlass, FaTrashCan } from "react-icons/fa6";
+import { FaRegEdit } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const MyParcels = () => {
 
     const { user } = UseAuth();
     const axiosSecure = UseAxiosSecure();
 
-    const { data: parcels = [] } = useQuery({
+    const { data: parcels = [], refetch } = useQuery({
         queryKey: ['myParcels', user?.email],
         queryFn: async () => {
             const res = await axiosSecure.get(`/parcels?email=${user?.email}`);
             return res.data;
         }
     })
+
+    const handleParcelDelete = id => {
+        console.log(id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "This parcel will be deleted.",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "I agree!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/parcels/${id}`)
+                    .then(res => {
+                        // console.log(res.data);
+
+                        if (res.data.deletedCount) {
+                            // auto refetching after delete
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted",
+                                text: "Your selected parcel is deleted.",
+                                icon: "success"
+                            });
+                        }
+
+                    })
+
+
+            }
+        });
+    }
 
     return (
         <div>
@@ -41,7 +78,17 @@ const MyParcels = () => {
                                     <td>{parcel.cost}</td>
                                     <td>Unpaid</td>
                                     <td>
-                                        
+                                        <button className='btn btn-square hover:bg-primary'>
+                                            <FaMagnifyingGlass></FaMagnifyingGlass>
+                                        </button>
+                                        <button className='btn btn-square hover:bg-primary mx-2'>
+                                            <FaRegEdit />
+                                        </button>
+                                        <button
+                                            onClick={() => handleParcelDelete(parcel._id)}
+                                            className='btn btn-square hover:bg-primary'>
+                                            <FaTrashCan />
+                                        </button>
                                     </td>
                                 </tr>
                             )}
