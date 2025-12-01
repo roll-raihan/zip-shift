9b5b099e9c87ce22bbaac9e99a9047b54849c62a
roll-raihan <raihanahmednasim@gmail.com>
2025-12-01 09:56:59 +0600

    ongoing payment method in client

--- src/layouts/DashboardLayout/DashboardLayout.jsx
+++ src/layouts/DashboardLayout/DashboardLayout.jsx
@@ -4,11 +4,11 @@ import { Link, NavLink, Outlet } from 'react-router';
 
 const DashboardLayout = () => {
     return (
-        <div className="drawer lg:drawer-open max-w-7xl mx-auto">
+        <div className="drawer lg:drawer-open max-w-7xl mx-auto bg-[#EAECED]">
             <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
                 {/* Navbar */}
-                <nav className="navbar w-full bg-base-300">
+                <nav className="navbar w-full bg-white">
                     <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost">
                         {/* Sidebar toggle icon */}
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor" className="my-1.5 inline-block size-4"><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path><path d="M9 4v16"></path><path d="M14 10l2 2l-2 2"></path></svg>
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import UseAxiosSecure from '../../../hooks/UseAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const Payment = () => {
+
+    const { parcelID } = useParams();
+    const axiosSecure = UseAxiosSecure();
+
+    const { isLoading, data: parcel } = useQuery({
+        queryKey: ['parcels', parcelID],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/parcels/${parcelID}`);
+            return res.data;
+        }
+    });
+
+    if (isLoading) {
+        return <div className='flex justify-center items-center'>
+            <span className="loading loading-infinity loading-xl"></span>
+        </div>
+    }
+
     return (
-        <div>
-            <h2>Please, pay!</h2>
+        <div className='bg-white rounded-2xl overflow-hidden m-5 p-10'>
+            <h2>Please pay for: {parcel.parcelName}</h2>
+            <button className="btn btn-primary text-black">Pay</button>
         </div>
     );
 };
