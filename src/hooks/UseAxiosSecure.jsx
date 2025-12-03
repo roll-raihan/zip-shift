b634bc5980e6a57643a9d55180cb4915da011efe
roll-raihan <raihanahmednasim@gmail.com>
2025-12-03 16:39:04 +0600

    verify token middleware and logout unauthorized access

--- src/hooks/UseAxiosSecure.jsx
+++ src/hooks/UseAxiosSecure.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
 import UseAuth from './UseAuth';
+import { useNavigate } from 'react-router';
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:3000',
@@ -10,12 +11,40 @@ const axiosSecure = axios.create({
 const UseAxiosSecure = () => {
 
     // intercept request
-    const { user } = UseAuth();
+    const { user, logOut } = UseAuth();
+    const navigate = useNavigate();
+
     useEffect(() => {
-        axiosSecure.interceptors.request.use(config => {
-            config.headers.Authorization=`Bearer ${user?.accessToken}`;
+        // interceptor request
+        const reqInterceptor = axiosSecure.interceptors.request.use(config => {
+            config.headers.Authorization = `Bearer ${user?.accessToken}`;
             return config;
         })
+
+        // interceptor response
+        const resInterceptor = axiosSecure.interceptors.response.use((response) => {
+            return response;
+        },
+            (error) => {
+                console.log(error);
+
+                const statusCode = error.status;
+                if (statusCode === 401 || statusCode === 403) {
+                    logOut()
+                        .then(() => {
+                            navigate('/login');
+                        })
+                }
+
+                return Promise.reject(error);
+            }
+        )
+
+        return () => {
+            axiosSecure.interceptors.request.eject(reqInterceptor);
+            axiosSecure.interceptors.response.eject(resInterceptor);
+        }
+
     }, [user])
 
     return axiosSecure;
